788f075295e240fdf11a09be2a9657be7cf11795
JulianMayorga <mayorga.julian@gmail.com>
2016-09-03 13:22:49 -0300

    Make PlaylistForm flex-ible

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { mergeClassNames } from '../utils/styles';
+
 import './Input.css';
 
-const Input = props => <input {...props} className='Input' />
+const Input = props => {
+    const className = mergeClassNames(props.className, 'Input');
+    return <input {...props} className={className} />;
+};
 
 export default Input;
