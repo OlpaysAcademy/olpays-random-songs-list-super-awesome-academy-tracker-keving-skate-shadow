eaaca0a563f474373253da21b3692967c54c00e9
JulianMayorga <mayorga.julian@gmail.com>
2016-09-05 09:13:07 -0300

    Refactor playlists logic into a module

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import FlipMove from 'react-flip-move';
-import shuffle from 'lodash/shuffle';
 import classNames from 'classnames';
 
 import Playlist from './Playlist';
+import { createPlaylists, addPlaylist, shufflePlaylists, increaseTimesPlayed, toggleBlacklist, isPlaylistVisible } from '../playlists';
 
 import './Playlists.css';
 
@@ -64,12 +64,6 @@ class Playlists extends Component {
     }
 }
 
-function shufflePlaylists(playlists) {
-    const blacklisted = playlists.filter(playlist => playlist.isBlacklisted);
-    const nonBlacklisted = playlists.filter(playlist => !playlist.isBlacklisted);
-    return shuffle(nonBlacklisted).concat(blacklisted);
-}
-
 function renderPlaylist(playlist) {
     const className = classNames({
         'Playlists-playlist-hidden': !isPlaylistVisible(playlist, this.state.timesPlayedMinimum)
@@ -83,54 +77,6 @@ function renderPlaylist(playlist) {
             key={playlist.id}
             />
     );
-}
-
-function createPlaylists() {
-    return [];
-}
-
-function addPlaylist(playlists, title) {
-    return playlists.concat([createPlaylist(title)]);
-}
-
-function createPlaylist(title) {
-    return {
-        title,
-        id: Math.random(),
-        timesPlayed: 0,
-        isVisible: true,
-        isBlacklisted: false
-    };
-}
-
-function isPlaylistVisible(playlist, timesPlayed) {
-    return playlist.timesPlayed >= timesPlayed;
-}
-
-function increaseTimesPlayed(playlists, id) {
-    return updatePlaylist(
-        playlists,
-        id,
-        playlist => {
-            playlist.timesPlayed += 1
-            return playlist;
-        }
-    );
-}
-
-function toggleBlacklist(playlists, id) {
-    return updatePlaylist(
-        playlists,
-        id,
-        playlist => {
-            playlist.isBlacklisted = !playlist.isBlacklisted;
-            return playlist;
-        }
-    );
-}
-
-function updatePlaylist(playlists, id, updater) {
-    return playlists.map(playlist => playlist.id === id ? updater(playlist) : playlist);
 }
 
 export default Playlists;
---
+++ src/playlists.js
@@ -0,0 +1,55 @@
+import shuffle from 'lodash/shuffle';
+
+export function shufflePlaylists(playlists) {
+    const blacklisted = playlists.filter(playlist => playlist.isBlacklisted);
+    const nonBlacklisted = playlists.filter(playlist => !playlist.isBlacklisted);
+    return shuffle(nonBlacklisted).concat(blacklisted);
+}
+
+export function createPlaylists() {
+    return [];
+}
+
+export function addPlaylist(playlists, title) {
+    return playlists.concat([createPlaylist(title)]);
+}
+
+function createPlaylist(title) {
+    return {
+        title,
+        id: Math.random(),
+        timesPlayed: 0,
+        isVisible: true,
+        isBlacklisted: false
+    };
+}
+
+export function isPlaylistVisible(playlist, timesPlayed) {
+    return playlist.timesPlayed >= timesPlayed;
+}
+
+export function increaseTimesPlayed(playlists, id) {
+    return updatePlaylist(
+        playlists,
+        id,
+        playlist => {
+            playlist.timesPlayed += 1
+            return playlist;
+        }
+    );
+}
+
+export function toggleBlacklist(playlists, id) {
+    return updatePlaylist(
+        playlists,
+        id,
+        playlist => {
+            playlist.isBlacklisted = !playlist.isBlacklisted;
+            return playlist;
+        }
+    );
+}
+
+function updatePlaylist(playlists, id, updater) {
+    return playlists.map(playlist => playlist.id === id ? updater(playlist) : playlist);
+}
